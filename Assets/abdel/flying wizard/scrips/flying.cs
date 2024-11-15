e7d79e87a08aaa364e39b4182cf1e3e51374fb54
abdel <abdelzozo971@gmail.com>
2024-11-15 22:51:13 +0400

    fixing mistakes made

--- Assets/abdel/flying wizard/scrips/flying.cs
+++ Assets/abdel/flying wizard/scrips/flying.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class flying : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [SerializeField] public float _velocity = 1.5f;
+
+     public Rigidbody2D rb;
+   
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+            rb.linearVelocity = Vector2.up * _velocity;
     }
 }
