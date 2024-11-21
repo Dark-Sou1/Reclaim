cadd00bc16a3ae54d7380f3e27a49ab84683e532
Giacomo <giacomo.rubboli@student.saedubai.com>
2024-11-22 00:03:21 +0400

    Reclaim text when map expands

---
+++ Assets/TowerDefense/Scripts/TD/DisableAfterWave.cs
@@ -0,0 +1,21 @@
+using Giacomo;
+using UnityEngine;
+
+public class DisableAfterWave : MonoBehaviour
+{
+    private void Awake()
+    {
+        WaveManager.Instance.SpawningNewWave += OnSpawningNewWave;
+    }
+
+    protected void OnSpawningNewWave(int wave)
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        if(WaveManager.Instance)
+            WaveManager.Instance.SpawningNewWave -= OnSpawningNewWave;
+    }
+}
