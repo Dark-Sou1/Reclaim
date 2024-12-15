1526a4841a23bcbb085d63fe1f1878217c7b9717
Dark-Sou1 <Aditya>
2024-12-15 09:20:41 +0400

    Added sound effects

--- Assets/Alchemy/Scripts/Item/ItemCollision.cs
+++ Assets/Alchemy/Scripts/Item/ItemCollision.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 public class ItemCollision : MonoBehaviour
 {
     public GameManager gameManager;
+    public AudioSource audioSource;
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("AetherFruit"))
         {
             gameManager.CollectItem("AetherFruit");
-            Destroy(other.gameObject); // Destroy the collected item
+            audioSource.Play();
+            Destroy(other.gameObject);
         }
         else if (other.CompareTag("Elixir Berries"))
         {
             gameManager.CollectItem("Elixir Berries");
+            audioSource.Play();
             Destroy(other.gameObject);
         }
         else if (other.CompareTag("Philosopher's Core"))
         {
             gameManager.CollectItem("Philosopher's Core");
+            audioSource.Play();
             Destroy(other.gameObject);
         }
     }
